872fbbc645cc9b924b920a476054f453cffebbc0
alex.chen <chenchangyuan5926@163.com>
2020-12-20 12:42:17 +0800

    --amend
    add insertion sort

--- insertion_sort/insertion_sort_test.go
+++ insertion_sort/insertion_sort_test.go
@@ -1,1 +1,29 @@
 package insertion_sort
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInsertionSort(t *testing.T) {
+	list := []int{1,3,5,2,4}
+	InsertionSort(list)
+	fmt.Println(list)
+}
+
+func InsertionSort(nums []int) {
+	for i := 1; i < len(nums); i++ {
+		cur := nums[i]
+		j := i - 1
+		if cur < nums[j] {
+			// 需要排序
+			for ; j > 0 && cur < nums[j]; j-- {
+				nums[j+1] = nums[j]
+			}
+			nums[j+1] = cur
+		}
+	}
+}
+
+// 1 3 5 2 4
+// 1 3 5
